50a8127775d8c17c1bc554fb0647b7d37044b101
Cody <cgregory315+git@gmail.com>
2026-08-25 11:50:41 -0400

    Section 3

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
 
+// This is the root layout of the app. You can edit this file to change the layout of the app's main screen. Stack is a component that allows you to navigate between screens in a stack-based manner.
 export default function RootLayout() {
   return <Stack />;
 }
